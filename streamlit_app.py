0c2a5faabd3e0090f118b65913d00ae0bf7bea0d
gellyacer <142211054+gellyacer@users.noreply.github.com>
2023-08-14 17:51:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mommy's New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
